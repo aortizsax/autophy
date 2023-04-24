480cf6663fbae2e91e1080002ed06c827e0dec3c
Adrian Ortiz Velez <aortizsax@gmail.com>
2023-04-24 16:30:38 -0700

    Updating trees and clean

--- scripts/2021-03-24_BEAST_branchlengths.R
+++
@@ -1,6 +0,0 @@
-install.packages("remotes")
-remotes::install_github("fmichonneau/phyloch")
-
-library(ape)
-read.nexus("../data/BSHaln/2021-03-21_BSH_filtered_muscle_relaxed.tree2021-03-21_BSH_filtered_muscle_relaxed.tree")
-
